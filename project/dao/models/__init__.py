6fe5c02d28bb093b90d44f7df85524b70e90e3f4
beth <besspar@gmail.com>
2022-03-22 23:42:46 +0300

    First commit

--- project/dao/models/__init__.py
+++ project/dao/models/__init__.py
@@ -1,5 +1,13 @@
 from .genre import Genre
+from .movie import Movie
+from .director import Director
+from .user import User
+from .favorite_movie import FavoriteMovie
 
 __all__ = [
     "Genre",
+    "Director",
+    "Movie",
+    "User",
+    "FavoriteMovies"
 ]
